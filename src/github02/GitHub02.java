1b4879a6db9aa74b49e614b8b407bf29d4f053e6
Mat <Mat@172.29.84.238>
2016-01-21 09:43:11 -0500

    Initial gitHub push.

---
+++ src/github02/GitHub02.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github02;
+
+/**
+ *
+ * @author Mat
+ */
+public class GitHub02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s = "Hello Peoples.";
+        System.out.printf("The phrase of the day is:%s%n", s);
+    }
+    
+}
